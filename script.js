dd4b5d14119cccb5c6fe3ff7bb1d141f4957bf2e
Moviehaul <barelyagamer1@gmail.com>
2026-07-08 10:58:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,405 +1,75 @@
 const movies = "a38759433da8160e11e7287ca0823446";
 
 const movieContainer = document.getElementById("movie-container");
+let currentPage = 1;
+function loadMovies(page) {
 
-fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${movies}&page=1`)
-    .then(response => response.json())
-    .then(data => {
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${movies}&certification_country=US&certification.lte=PG-13&sort_by=popularity.desc&page=${page}`)
+        .then(response => response.json())
+        .then(data => {
 
-        data.results.forEach(movie => {
+            data.results.forEach(movie => {
 
-            const card = document.createElement("a");
+                const card = document.createElement("a");
 
-            card.href = `movie.html?id=${movie.id}`;
+                card.href = `movie.html?id=${movie.id}`;
 
-            card.innerHTML = `
-                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-                     alt="${movie.title}">
-            `;
+                card.innerHTML = `
+                    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+                         alt="${movie.title}">
+                `;
 
-            movieContainer.appendChild(card);
+                movieContainer.appendChild(card);
 
-        });
-
-    });
-fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${movies}&page=2`)
-    .then(response => response.json())
-    .then(data => {
-
-        data.results.forEach(movie => {
-
-            const card = document.createElement("a");
-
-            card.href = `movie.html?id=${movie.id}`;
-
-            card.innerHTML = `
-                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-                     alt="${movie.title}">
-            `;
-
-            movieContainer.appendChild(card);
+            });
 
         });
+}
+loadMovies(currentPage);
+window.addEventListener("scroll", () => {
+
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 100) {
+
+        currentPage++;
+
+        loadMovies(currentPage);
+
+    }
+
+});
+async function searchMovies() {
+
+    const query = document.getElementById("searchInput").value;
+
+    if (!query) return;
+
+    const response = await fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=${movies}&query=${query}`
+    );
+
+    const data = await response.json();
+
+    displayMovies(data.results);
+}
+
+
+function displayMovies(results) {
+
+    movieContainer.innerHTML = "";
+
+    results.forEach(movie => {
+
+        const card = document.createElement("a");
+
+        card.href = `movie.html?id=${movie.id}`;
+
+        card.innerHTML = `
+            <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+                 alt="${movie.title}">
+        `;
+
+        movieContainer.appendChild(card);
 
     });
-fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${movies}&page=3`)
-    .then(response => response.json())
-    .then(data => {
 
-        data.results.forEach(movie => {
-
-            const card = document.createElement("a");
-
-            card.href = `movie.html?id=${movie.id}`;
-
-            card.innerHTML = `
-                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-                     alt="${movie.title}">
-            `;
-
-            movieContainer.appendChild(card);
-
-        });
-
-    });
-fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${movies}&page=4`)
-    .then(response => response.json())
-    .then(data => {
-
-        data.results.forEach(movie => {
-
-            const card = document.createElement("a");
-
-            card.href = `movie.html?id=${movie.id}`;
-
-            card.innerHTML = `
-                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-                     alt="${movie.title}">
-            `;
-
-            movieContainer.appendChild(card);
-
-        });
-
-    });
-fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${movies}&page=5`)
-    .then(response => response.json())
-    .then(data => {
-
-        data.results.forEach(movie => {
-
-            const card = document.createElement("a");
-
-            card.href = `movie.html?id=${movie.id}`;
-
-            card.innerHTML = `
-                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-                     alt="${movie.title}">
-            `;
-
-            movieContainer.appendChild(card);
-
-        });
-
-    });
-fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${movies}&page=6`)
-    .then(response => response.json())
-    .then(data => {
-
-        data.results.forEach(movie => {
-
-            const card = document.createElement("a");
-
-            card.href = `movie.html?id=${movie.id}`;
-
-            card.innerHTML = `
-                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-                     alt="${movie.title}">
-            `;
-
-            movieContainer.appendChild(card);
-
-        });
-
-    });
-fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${movies}&page=7`)
-    .then(response => response.json())
-    .then(data => {
-
-        data.results.forEach(movie => {
-
-            const card = document.createElement("a");
-
-            card.href = `movie.html?id=${movie.id}`;
-
-            card.innerHTML = `
-                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-                     alt="${movie.title}">
-            `;
-
-            movieContainer.appendChild(card);
-
-        });
-
-    });
-fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${movies}&page=8`)
-    .then(response => response.json())
-    .then(data => {
-
-        data.results.forEach(movie => {
-
-            const card = document.createElement("a");
-
-            card.href = `movie.html?id=${movie.id}`;
-
-            card.innerHTML = `
-                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-                     alt="${movie.title}">
-            `;
-
-            movieContainer.appendChild(card);
-
-        });
-
-    });
-fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${movies}&page=9`)
-    .then(response => response.json())
-    .then(data => {
-
-        data.results.forEach(movie => {
-
-            const card = document.createElement("a");
-
-            card.href = `movie.html?id=${movie.id}`;
-
-            card.innerHTML = `
-                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-                     alt="${movie.title}">
-            `;
-
-            movieContainer.appendChild(card);
-
-        });
-
-    });
-fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${movies}&page=10`)
-    .then(response => response.json())
-    .then(data => {
-
-        data.results.forEach(movie => {
-
-            const card = document.createElement("a");
-
-            card.href = `movie.html?id=${movie.id}`;
-
-            card.innerHTML = `
-                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-                     alt="${movie.title}">
-            `;
-
-            movieContainer.appendChild(card);
-
-        });
-
-    });
-fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${movies}&page=11`)
-    .then(response => response.json())
-    .then(data => {
-
-        data.results.forEach(movie => {
-
-            const card = document.createElement("a");
-
-            card.href = `movie.html?id=${movie.id}`;
-
-            card.innerHTML = `
-                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-                     alt="${movie.title}">
-            `;
-
-            movieContainer.appendChild(card);
-
-        });
-
-    });
-fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${movies}&page=12`)
-    .then(response => response.json())
-    .then(data => {
-
-        data.results.forEach(movie => {
-
-            const card = document.createElement("a");
-
-            card.href = `movie.html?id=${movie.id}`;
-
-            card.innerHTML = `
-                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-                     alt="${movie.title}">
-            `;
-
-            movieContainer.appendChild(card);
-
-        });
-
-    });
-fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${movies}&page=13`)
-    .then(response => response.json())
-    .then(data => {
-
-        data.results.forEach(movie => {
-
-            const card = document.createElement("a");
-
-            card.href = `movie.html?id=${movie.id}`;
-
-            card.innerHTML = `
-                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-                     alt="${movie.title}">
-            `;
-
-            movieContainer.appendChild(card);
-
-        });
-
-    });
-fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${movies}&page=14`)
-    .then(response => response.json())
-    .then(data => {
-
-        data.results.forEach(movie => {
-
-            const card = document.createElement("a");
-
-            card.href = `movie.html?id=${movie.id}`;
-
-            card.innerHTML = `
-                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-                     alt="${movie.title}">
-            `;
-
-            movieContainer.appendChild(card);
-
-        });
-
-    });
-fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${movies}&page=15`)
-    .then(response => response.json())
-    .then(data => {
-
-        data.results.forEach(movie => {
-
-            const card = document.createElement("a");
-
-            card.href = `movie.html?id=${movie.id}`;
-
-            card.innerHTML = `
-                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-                     alt="${movie.title}">
-            `;
-
-            movieContainer.appendChild(card);
-
-        });
-
-    });
-fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${movies}&page=16`)
-    .then(response => response.json())
-    .then(data => {
-
-        data.results.forEach(movie => {
-
-            const card = document.createElement("a");
-
-            card.href = `movie.html?id=${movie.id}`;
-
-            card.innerHTML = `
-                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-                     alt="${movie.title}">
-            `;
-
-            movieContainer.appendChild(card);
-
-        });
-
-    });
-fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${movies}&page=17`)
-    .then(response => response.json())
-    .then(data => {
-
-        data.results.forEach(movie => {
-
-            const card = document.createElement("a");
-
-            card.href = `movie.html?id=${movie.id}`;
-
-            card.innerHTML = `
-                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-                     alt="${movie.title}">
-            `;
-
-            movieContainer.appendChild(card);
-
-        });
-
-    });
-fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${movies}&page=18`)
-    .then(response => response.json())
-    .then(data => {
-
-        data.results.forEach(movie => {
-
-            const card = document.createElement("a");
-
-            card.href = `movie.html?id=${movie.id}`;
-
-            card.innerHTML = `
-                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-                     alt="${movie.title}">
-            `;
-
-            movieContainer.appendChild(card);
-
-        });
-
-    });
-fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${movies}&page=19`)
-    .then(response => response.json())
-    .then(data => {
-
-        data.results.forEach(movie => {
-
-            const card = document.createElement("a");
-
-            card.href = `movie.html?id=${movie.id}`;
-
-            card.innerHTML = `
-                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-                     alt="${movie.title}">
-            `;
-
-            movieContainer.appendChild(card);
-
-        });
-
-    });
-fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${movies}&page=20`)
-    .then(response => response.json())
-    .then(data => {
-
-        data.results.forEach(movie => {
-
-            const card = document.createElement("a");
-
-            card.href = `movie.html?id=${movie.id}`;
-
-            card.innerHTML = `
-                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-                     alt="${movie.title}">
-            `;
-
-            movieContainer.appendChild(card);
-
-        });
-
-    });
-    
+}
